afe212aa400ed90998d946f3ec75618a711634af
MasaokiSeta <80959203+MasaokiSeta@users.noreply.github.com>
2022-09-16 08:29:30 +0900

    Chapter7完了

--- Gemfile
+++ Gemfile
@@ -75,3 +75,7 @@ gem 'bootstrap'
 gem 'rails_autolink'
 
 gem "ransack"
+
+gem "kaminari"
+
+gem "sidekiq"
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,12 @@ class TasksController < ApplicationController
   def index
     # インスタンス変数はview側でもアクセス出来る筈
     @q = current_user.tasks.ransack(params[:q])
-    @tasks = @q.result(distinct: true)
+    @tasks = @q.result(distinct: true).page(params[:page])
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @tasks.generate_csv, filename: "tasks-#{Time.zone.now.strftime('%Y%m%d%S')}.csv"}
+    end
   end
 
   def show
@@ -33,6 +38,7 @@ class TasksController < ApplicationController
     if @task.save
       TaskMailer.creation_email(@task).deliver_now
       #TaskMailer.creation_email(@task).deliver_later(wait: 1.minutes)
+      SampleJob.perform_later
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
@@ -50,6 +56,11 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
+  end
+
+  def import
+    current_user.tasks.import(params[:file])
+    redirect_to tasks_url, notice: "タスクを追加しました"
   end
 
   private 
